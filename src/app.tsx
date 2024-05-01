4b0d118262ce6d8de6cb31b30d2b15834ae2d7ba
Jecília Teixeira <111911408+Jecilia@users.noreply.github.com>
2024-05-01 21:57:26 +0100

    ...

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,15 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { Router } from './router'
+import { BrowserRouter } from 'react-router-dom'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <p>ik</p>
-      <h1>yh</h1>
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
