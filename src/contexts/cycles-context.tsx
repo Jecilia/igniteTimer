6710cfa29fc1ab05d928e38fab4d45141fbbe963
Jecília Teixeira <111911408+Jecilia@users.noreply.github.com>
2024-05-22 23:58:31 +0100

    ...

--- src/contexts/cycles-context.tsx
+++ src/contexts/cycles-context.tsx
@@ -1,4 +1,10 @@
-import { ReactNode, createContext, useReducer, useState } from 'react'
+import {
+  ReactNode,
+  createContext,
+  useEffect,
+  useReducer,
+  useState,
+} from 'react'
 import { CiclesReducer, cycle } from '../reducers/cycles/reducer'
 import {
   AddNewCyclesActions,
@@ -29,12 +35,28 @@ interface CyclesContextProviderProps {
 export function CyclesContextProvider({
   children,
 }: CyclesContextProviderProps) {
-  const [cyclesState, dispatch] = useReducer(CiclesReducer, {
-    cycles: [],
-    activeCycleId: null,
-  })
+  const [cyclesState, dispatch] = useReducer(
+    CiclesReducer,
+    {
+      cycles: [],
+      activeCycleId: null,
+    },
+    () => {
+      const storedStateAsJSON = localStorage.getItem(
+        '@igniteTimer: cycles-state-1.0.0',
+      )
+      if (storedStateAsJSON) {
+        return JSON.parse(storedStateAsJSON)
+      }
+    },
+  )
 
   const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+  useEffect(() => {
+    const stateJSON = JSON.stringify(cyclesState)
+    localStorage.setItem('@igniteTimer: cycles-state-1.0.0', stateJSON)
+  }, [cyclesState])
+
   const { cycles, activeCycleId } = cyclesState
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
--- src/pages/history/index.tsx
+++ src/pages/history/index.tsx
@@ -32,13 +32,13 @@ export function History() {
                   </td>
                   <td>
                     {cycle.finishedDate && (
-                      <Status statusColor="green">Concluido</Status>
+                      <Status statuscolor="green">Concluido</Status>
                     )}
                     {cycle.interruptedDate && (
-                      <Status statusColor="red">Interrompido</Status>
+                      <Status statuscolor="red">Interrompido</Status>
                     )}
                     {!cycle.finishedDate && !cycle.interruptedDate && (
-                      <Status statusColor="yellow">Em andamento</Status>
+                      <Status statuscolor="yellow">Em andamento</Status>
                     )}
                   </td>
                 </tr>
